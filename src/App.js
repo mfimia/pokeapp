c7711e047238974726d1d61e11e4d118518f0528
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-09 21:17:15 +0100

    added state, router and theme provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from "react";
+import { useMemo, useContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DarkContext from "./context/darkMode/DarkContext";
+import PokemonList from "./components/pokemon/PokemonList";
+import PokemonPage from "./components/pokemon/PokemonPage";
+import PokemonState from "./context/pokemon/PokemonState";
+import Navbar from "./components/layout/Navbar";
+import AlertState from "./context/alert/AlertState";
+import { Container, CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const darkContext = useContext(DarkContext);
+
+  const { mode } = darkContext;
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: { mode },
+      }),
+    [mode]
+  );
+
+  return (
+    <PokemonState>
+      <AlertState>
+        <Router>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container
+              maxWidth="lg"
+              sx={{
+                bgcolor: "background.default",
+                color: "text.primary",
+                px: 4,
+              }}
+            >
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<PokemonList />} />
+                <Route path="/poke" element={<PokemonPage />} />
+              </Routes>
+            </Container>
+          </ThemeProvider>
+        </Router>
+      </AlertState>
+    </PokemonState>
+  );
 };
 
 export default App;
