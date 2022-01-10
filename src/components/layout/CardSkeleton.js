034f338ff2440837a31a10e42cf97d22e2dacea4
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-10 08:45:20 +0100

    fixed skeleton width in large component

--- src/components/layout/CardSkeleton.js
+++ src/components/layout/CardSkeleton.js
@@ -4,7 +4,7 @@ import Stack from "@mui/material/Stack";
 const CardSkeleton = () => {
   return (
     <Stack spacing={2} m={2} px={2} pt={2}>
-      <Skeleton variant="text" />
+      <Skeleton variant="text" width={210} />
       <Skeleton variant="circular" width={40} height={40} />
       <Skeleton variant="rectangular" width={210} height={218} />
     </Stack>
