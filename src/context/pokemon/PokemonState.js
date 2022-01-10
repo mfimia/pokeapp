cbc47f7eb7b30211401573e99628175a67d2109d
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-10 08:47:07 +0100

    fixed pagination to dynamically calculate based on total count. added individual pokemon data to state

--- src/context/pokemon/PokemonState.js
+++ src/context/pokemon/PokemonState.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import PokemonContext from "./PokemonContext";
+import { P } from "../../utils/Pokedex";
 
 function PokemonState(props) {
   const [loading, setLoading] = useState(true);
+  const [pageCount, setPageCount] = useState(null);
 
   const [pokemonList, setPokemonList] = useState([]);
 
-  // Get pokemons
+  const [basicInfo, setBasicInfo] = useState({});
+  const [species, setSpecies] = useState({});
+
+  // Get list of pokemons
   const getPokemonList = async (offset = 0) => {
     setLoading(true);
     try {
@@ -15,9 +20,25 @@ function PokemonState(props) {
       );
       const pokemonData = await res.json();
       setPokemonList(pokemonData.results);
+      setPageCount(pokemonData.count);
       setLoading(false);
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  // Get data for individual pokemon
+  const getIndividualPokemonData = async (value) => {
+    try {
+      setLoading(true);
+      const basicInfoData = await P.getPokemonByName(value);
+      const speciesData = await P.getPokemonSpeciesByName(value);
+      setBasicInfo(basicInfoData);
+      setSpecies(speciesData);
+      setLoading(false);
+      return true;
+    } catch (error) {
+      return false;
     }
   };
 
@@ -26,8 +47,12 @@ function PokemonState(props) {
       value={{
         pokemonList,
         loading,
+        pageCount,
+        basicInfo,
+        species,
         getPokemonList,
         setPokemonList,
+        getIndividualPokemonData,
       }}
     >
       {props.children}
